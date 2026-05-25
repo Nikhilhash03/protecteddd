d934de2203a50756bb7ecd73ba839aa4208f2b83
Nikhil Razz <nikhilhash03@gmail.com>
2026-05-25 15:47:27 +0530

    Add project files.

---
+++ protecteddd/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protecteddd
+{
+    class myclass
+    {
+        protected int a = 1;
+        public void mymethod()
+        {
+            Console.WriteLine("Value of a under mymethod " + a);
+        }
+    }
+    class newclass : myclass
+    {
+        public void newmethod()
+        {
+            Console.WriteLine("value of a under newmethod " + a);
+            //base:mymethod();
+        }
+    }
+    internal class protecteddd
+    {
+        static void Main(string[] args)
+        {
+            newclass obj = new newclass();
+            obj.mymethod();
+            obj.newmethod();
+        }
+    }
+    }
