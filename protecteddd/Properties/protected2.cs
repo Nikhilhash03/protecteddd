d9c0cd29a7a026d256ee089a5ed3f219ea507d98
Nikhil Razz <nikhilhash03@gmail.com>
2026-05-25 16:12:20 +0530

    updated

--- protecteddd/Properties/protected2.cs
+++ protecteddd/Properties/protected2.cs
@@ -1,20 +1,22 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace protecteddd.Properties
-//{
-//    class nikhil : myclass
-//    {
-//        public void nikhilmethod()
-//        {
-//            Console.WriteLine("Value of a under nikhil class "+a);
-//        }
-       
-//    }
-//    internal class protected2
-//    {
-//    }
-//}
+namespace protecteddd.Properties
+{
+    class nikhil : newclass
+    {
+        public void nikhilmethod()
+        {
+            Console.WriteLine("Value of a under nikhil class " + a);
+            base.newmethod();
+            
+        }
+
+    }
+    internal class protected2
+    {
+    }
+}
